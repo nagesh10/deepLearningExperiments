d7b7007e9e0fce821b71b407c44f5884bf4abfc1
Vikram Voleti <vikram.voleti@gmail.com>
2017-03-29 22:26:18 +0530

    deleted irrelevant commented code

--- myKeras_FaceNet_NN1/myKeras_FaceNet_NN1_20170326_xxxxxx.py
+++ myKeras_FaceNet_NN1/myKeras_FaceNet_NN1_20170326_xxxxxx.py
@@ -1,10 +1,6 @@
-# Make sure to run the code from same directory itself,
-# like :       python myKerasSmallNN_autoencoder_2layers_xxxxxx_xxxxxx.py
-# instead of : python /projects/myKerasNN/myKerasSmallNN_autoencoder_2layers.py
 print "myKeras_FaceNet_NN1.py"
 
 '''
-
 # Paths
 basePath = os.getcwd()
 modelsPath = basePath + '/../MODELS'
@@ -142,6 +138,7 @@ nn1.add(Lambda(lambda x: K.l2_normalize(x, axis=1)))
 # Compile the model
 nn1.compile(optimizer='adam', loss='binary_crossentropy')
 
+'''
 # Fit the model
 nn1.fit(newTrainImages, newTrainImages,
                 batch_size=batchSize,
@@ -158,126 +155,8 @@ modelName = 'myKerasSmallNNModel_autoencoder_2layers_' + timeStr + '.h5'
 nn1.save(modelName)
 print "model saved."
 
-'''
 # Load the model
 modelName = modelsPath + '/myKerasSmallNNModel_autoencoder_2layers_20161112_184710.h5'
 autoencoder = load_model(modelName)
 '''
 
-# FINDING RANGE OF ENTROPIES
-
-# calc entropy for train
-trainPreds = nn1.predict(newTrainImages)
-trainEntropies = calcCrossEntropy(newTrainImages, trainPreds, nbBins)
-
-'''
-for i in range(trainPreds.shape[0]):
-	plotImage = np.concatenate((newTrainImages[i].reshape(imRows, imCols), trainPreds[i].reshape(imRows, imCols)), axis=1)
-	plt.imshow(plotImage, cmap='gray')
-	plt.show()
-'''
-
-'''
-# predict, calc entropy for val
-valPreds = nn1.predict(newValImages)
-valEntropies = calcCrossEntropy(newValImages, valPreds, nbBins)
-
-# predict, calc entropy for test
-testPreds = nn1.predict(newTestImages)
-testEntropies = calcCrossEntropy(newTestImages, testPreds, nbBins)
-
-
-# Entropies
-
-print "trainEntropies : " + trainEntropies.min().astype(str) + " ---- " + trainEntropies.max().astype(str)
-print "valEntropies   : " + valEntropies.min().astype(str) + " ---- " + valEntropies.max().astype(str)
-print "testEntropies  : " + testEntropies.min().astype(str) + " ---- " + testEntropies.max().astype(str)
-
-minEntropy = np.array([trainEntropies.min(), valEntropies.min(), testEntropies.min()]).min()
-maxEntropy = np.array([trainEntropies.max(), valEntropies.max(), testEntropies.max()]).max()
-
-minEntropy = -2.62555459586
-maxEntropy = -1.87461698778
-
-# TESTING
-
-[origImRows, origImCols, origImChannels, (trainImages, trainResults), (valImages, valResults), (testImages, testResults), fullTestImages] = loadData(2, date)
-newTrainImages = resizeImages(trainImages, imRows, imCols)
-newValImages = resizeImages(valImages, imRows, imCols)
-newTestImages = resizeImages(testImages, imRows, imCols)
-
-# train
-trainPreds = nn1.predict(newTrainImages)
-trainEntropies = calcCrossEntropy(newTrainImages, trainPreds, nbBins)
-
-trainClass = ((trainEntropies>=minEntropy) & (trainEntropies<=maxEntropy)).astype(int)
-
-trainClassAccuracy = ((trainClass==trainResults).astype(int).sum())/float(trainClass.shape[0])
-
-# full test
-
-newFullTestImages = resizeImages(fullTestImages, imRows, imCols)
-fullTestPreds = nn1.predict(newFullTestImages, imRows, imCols)
-
-fullTestEntropies = calcCrossEntropy(newFullTestImages, fullTestPreds)
-fulltestClass = ((fullTestEntropies>=minEntropy) & (fullTestEntropies<=maxEntropy)).astype(int)
-
-fullTestClassAccuracy = ((fulltestClass==trainResults).astype(int).sum())/float(trainClass.shape[0])
-
-print
-
-for i in range(fullTestEntropies.shape[0]):
-    #plotImage = fullTestImages[i].reshape(origImRows, origImCols)
-    plotImage = np.concatenate((newFullTestImages[i].reshape(imRows, imCols), fullTestPreds[i].reshape(imRows, imCols)), axis=1)
-    plt.imshow(plotImage, cmap='gray')
-    rangeText = "Entropy range : " + str(minEntropy) + " ---- " + str(maxEntropy)
-    text(2, 2, rangeText, color='black', fontsize=20)
-    text(2.5, 2.5, rangeText, color='white', fontsize=20)
-    iEntropyText = fullTestEntropies[i].astype(str)
-    text(2, 10, iEntropyText, color='black', fontsize=20)
-    text(2.5, 10.5, iEntropyText, color='white', fontsize=20)
-    if (fullTestEntropies[i]>=minEntropy)&(fullTestEntropies[i]<=maxEntropy) :
-        state='EMPTY_CASKET'
-    else:
-        state='NOT Empty_casket'
-    text(2, 18, state, color='black', fontsize=20)
-    text(2.5, 18.5, state, color='white', fontsize=20)
-    plt.show()
-
-'''
-
-'''
-from keras.datasets import mnist
-(x_train, _), (x_test, _) = mnist.load_data()
-
-x_train = x_train.astype('float32') / 255.
-x_test = x_test.astype('float32') / 255.
-x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
-x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
-
-input_img = Input(shape=(28, 28, 1))
-
-x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(input_img)
-x = MaxPooling2D((2, 2), border_mode='same')(x)
-x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(x)
-encoded = MaxPooling2D((2, 2), border_mode='same')(x)
-
-# at this point the representation is (32, 7, 7)
-
-x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(encoded)
-x = UpSampling2D((2, 2))(x)
-x = Convolution2D(32, 3, 3, activation='relu', border_mode='same')(x)
-x = UpSampling2D((2, 2))(x)
-decoded = Convolution2D(1, 3, 3, activation='sigmoid', border_mode='same')(x)
-
-autoencoder = Model(input_img, decoded)
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
-
-autoencoder.fit(x_train, x_train,
-                nb_epoch=100,
-                batch_size=128,
-                shuffle=True,
-                validation_data=(x_test, x_test))
-
-
-'''
